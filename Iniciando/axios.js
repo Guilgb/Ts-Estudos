415b4ffcc372c9da7b5afa333ef9191c9be6f52d
guilherme <guilherme.gb@hotmail.com>
2022-10-27 10:40:13 -0300

    promises axios

--- Iniciando/axios.js
+++ Iniciando/axios.js
@@ -4,3 +4,12 @@ axios
     .get('https://api.github.com/users/guilgb')
     .then(response => axios.get(response.data.repos_url))
 
+
+Promise.all([
+    axios.get('https://api.github.com/users/guilgb'),
+    axios.get('https://api.github.com/users/guilgb')
+]).then(response => {
+    console.log(response[0].data.login)
+    console.log(response[1].data.login)
+    })
+
